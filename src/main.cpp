076566a917804d11af812668baae928e346e498c
Frogpants <spencerjl0309@gmail.com>
2026-06-08 01:28:31 -0700

    Replaced test tri with test player

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,48 @@
+#include <iostream>
+
 #include "core/render/window.hpp"
 #include "core/render/render.hpp"
+
+#include "core/math/essentials.hpp"
+
+vec2 player = vec2(0.0);
+vec2 vel = vec2(0.0);
 
 int main() {
     Window w = Window(1280, 720, "Red Noise");
     Render render;
     render.init();
 
-    render.clear = vec4(vec3(0.5), 1.0);
+    float aspect = 1280.0/720.0;
+    float speed = 0.001;
+
+    int tick = 0;
 
     while (w.isOpen()) {
         w.pollEvents();
         render.beginFrame();
 
-        //render.drawQuad(vec2(-0.5, -0.5), vec2(-0.5, 0.5), vec2(0.5, 0.5), vec2(0.5, -0.5), vec4(vec3(0.0), 1.0));
-        render.drawRect(vec2(0.0), vec2(0.25, 0.25), vec4(vec3(0.0), 1.0));
+        tick += 1;
 
+        if (tick % 5 == 0) {
+            if (w.isKeyPressed("w")) {
+                vel.y += speed;
+            }
+            if (w.isKeyPressed("s")) {
+                vel.y += -speed;
+            }
+            if (w.isKeyPressed("d")) {
+                vel.x += speed;
+            }
+            if (w.isKeyPressed("a")) {
+                vel.x += -speed;
+            }
 
+            vel = vel * 0.8;
+            player = player + vel;
+        }
+
+        render.drawRect(player, vec2(0.05 / aspect, 0.05), vec4(vec3(0.0), 1.0));
 
         if (w.isKeyPressed("esc")) {
             break;
