0e450f5e9adf7b1bfbc4ca8c8925f19e14fd1607
Frogpants <spencerjl0309@gmail.com>
2026-06-23 21:18:17 -0700

    updated vert shader

--- src/core/render/shaders/vert.glsl
+++ src/core/render/shaders/vert.glsl
@@ -4,11 +4,17 @@ layout(location = 0) in vec2 position;
 layout(location = 1) in vec4 color;
 layout(location = 2) in vec2 uv;
 
+in uniform vec3 cam;
+in uniform vec3 rot;
+in uniform float f;
+
 out vec4 vertexColor;
 out vec2 vUV;
 
 void main()
 {
+    
+
     gl_Position = vec4(position, 0.0, 1.0);
 
     vertexColor = color;
